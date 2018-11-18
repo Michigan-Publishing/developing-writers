fe3a6f2b1f8a897a613382007e9b3b7c5e6bb0ef
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2018-11-18 10:45:07 -0500

    Test index page with a rough query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,81 @@
-import React from "react"
+import React from "react";
+import { graphql } from "gatsby";
 
-export default () => <div>Hello world!</div>
+const IndexPage = ({
+  data: {
+    allMdx: { edges }
+  }
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.key) // You can filter your posts based on some criteria
+    // we could technically just treat this as the filter too
+    // doing this for example and "simplicity"
+    .sort((a, b) => {
+      return a.node.frontmatter.parentKey === null ? -1 : 1;
+    })
+    .map(k => {
+      return {
+        title: k.node.frontmatter.title,
+        key: k.node.frontmatter.key,
+        parentKey: k.node.frontmatter.parentKey
+      };
+    });
+  // .reduce((edges, nextEdge) => {
+  //   // if this is a root element
+  //   if (nextEdge.parentKey === null) {
+  //     edges[nextEdge.node.frontmatter.key] = {
+  //       tite: nextEdge.node.fronmatter.title,
+  //       key: nextEdge.node.frontmatter.key
+  //     };
+  //     return edges;
+  //   }
+
+  //   // figure out if this is a child element - hasParent & parent exists
+  //   if (
+  //     nextEdge.frontmatter.parentKey &&
+  //     edges[nextEdge.node.frontmatter.parentKey]
+  //   ) {
+  //     edges[nextEdge.node.frontmatter.key].children.push({
+  //       tite: nextEdge.node.fronmatter.title,
+  //       key: nextEdge.node.frontmatter.key
+  //     });
+  //     return edges;
+  //   }
+  // }, {});
+
+  // this is garbage - the reduce should work instead
+  // also - not totally sure how to get hte link. I see a lot of
+  // people have the path to the resource in frontmatter and they usually
+  // add that to the query
+  return (
+    <div>
+      {Posts.map(p =>
+        p.parentKey ? (
+          <div style={{ marginLeft: 10 }}>{p.title}</div>
+        ) : (
+          <div>{p.title}</div>
+        )
+      )}
+    </div>
+  );
+};
+
+export default IndexPage;
+
+export const pageQuery = graphql`
+  query {
+    allMdx {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
+            description
+            key
+            parentKey
+          }
+        }
+      }
+    }
+  }
+`;
