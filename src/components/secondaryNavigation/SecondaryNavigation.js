18284ecf70ec193cbcfee4ff0302c246c222c412
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2018-11-17 21:36:38 -0500

    Add secondary nav

--- src/components/secondaryNavigation/SecondaryNavigation.js
+++ src/components/secondaryNavigation/SecondaryNavigation.js
@@ -3,12 +3,24 @@ import styled from "styled-components";
 import palette from "../../utils/palette";
 import { textCss } from "../text/Text";
 
+const padding = 20;
 const Link = styled.a`
   color: ${palette.white};
   ${textCss};
+  font-size: 30px;
+  text-decoration: none;
+  position: flex;
+
+  &:after {
+    content: " ";
+    display: block;
+    border-bottom: 1px solid ${palette.white};
+    opacity: 0.5;
+  }
 `;
 
 const ListContainer = styled.ul`
+  padding: ${padding}px 0;
   display: flex;
   flex-direction: column;
   flex-grow: 1;
@@ -28,15 +40,35 @@ function getRandomInt(max) {
 }
 
 export default class extends Component {
+  state = { paddingHeight: 90 };
+  mounted = false;
+
+  componentDidMount() {
+    this.getPaddingAmount();
+    window.addEventListener("resize", this.getPaddingAmount);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.getPaddingAmount);
+  }
+
+  getPaddingAmount = () => {
+    const { linkProperties } = this.props;
+    const height = this.container.clientHeight / linkProperties.length;
+    this.setState({ paddingHeight: height > 90 ? 90 : height });
+  };
+
   render() {
     const { linkProperties } = this.props;
+    const { paddingHeight } = this.state;
+
     if (!linkProperties) {
       return null;
     }
     return (
-      <ListContainer>
+      <ListContainer ref={node => (this.container = node)}>
         {linkProperties.map((link, index) => (
-          <ListItem left={getRandomInt(35)} top={index * 90}>
+          <ListItem left={getRandomInt(35)} top={index * paddingHeight}>
             <Link href={link.href}>{link.title}</Link>
           </ListItem>
         ))}
