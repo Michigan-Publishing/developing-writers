69e0f722fea5be36eb1776583a21a413a3daa3d4
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2018-12-03 06:34:33 -0500

    Update background to make it much simpler

--- src/components/background/Background.js
+++ src/components/background/Background.js
@@ -1,7 +1,6 @@
 import React from "react";
-import styled from "styled-components";
 import standard from "./background.jpg";
-import webp from "./background.webp";
+import styled from "styled-components";
 
 const Background = styled.div`
   background: url(${props => props.src}) no-repeat center center fixed;
@@ -12,35 +11,7 @@ const Background = styled.div`
 `;
 
 const BackgroundWrapper = ({ children, src }) => (
-  <Background src={src}>{children}</Background>
+  <Background src={standard}>{children}</Background>
 );
 
-async function supportsWebp() {
-  if (!createImageBitmap) return false;
-
-  const webpData =
-    "data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=";
-  const blob = await fetch(webpData).then(r => r.blob());
-  return createImageBitmap(blob).then(() => true, () => false);
-}
-
-class BackgroundSourceWrapper extends React.Component {
-  state = { isLoading: true, src: null };
-
-  async componentDidMount() {
-    const useWebp = false;
-
-    this.setState({
-      isLoading: false,
-      src: useWebp ? webp : standard
-    });
-  }
-
-  render() {
-    const { isLoading, src } = this.state;
-
-    return isLoading ? null : <BackgroundWrapper {...this.props} src={src} />;
-  }
-}
-
-export default BackgroundSourceWrapper;
+export default BackgroundWrapper;
