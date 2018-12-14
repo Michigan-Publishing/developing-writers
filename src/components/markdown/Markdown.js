41f6df9a03c8ff7c0bb7c8eb95cf5c2657c53b09
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2018-12-13 22:46:53 -0500

    Testing some assumptions with Marksy

--- src/components/markdown/Markdown.js
+++ src/components/markdown/Markdown.js
@@ -1,17 +1,15 @@
 import React from "react";
 import marksy from "marksy/components";
 
+const ExpandableBlockquote = () => <h1>Hi</h1>;
+
 const compile = marksy({
   createElement: React.createElement,
   components: {
-    ExpandableBlockquote (props) {
-      console.log('EXPANDABLE PROPS');
-      console.log(props);
-      return <h1>Explandable</h1>;
-    }
+    ExpandableBlockquote
   }
 });
 
-const Markdown = ({ children }) => children && compile(children).tree;
+const Markdown = ({ children }) =>  children && compile(children).tree;
 
 export default Markdown;
