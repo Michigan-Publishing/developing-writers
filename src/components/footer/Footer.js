ed2cb609fb23975dbdd5a3a97c7eb130c6ca1d9e
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2018-12-16 14:55:37 -0500

    Update footer for mobile

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,16 +1,17 @@
 import React, { Component, Fragment } from "react";
 import styled from "styled-components";
-import { Link } from "gatsby";
+import { Link as GatsbyLink } from "gatsby";
 import palette from "../../utils/palette";
 import { textCss } from "../text/Text";
+import { TABLET_LANDSCAPE_WIDTH } from "../../constants";
 
 import logo from "./umich-logo.png";
 
 export const FOOTER_HEIGHT = 172;
 
 const FooterWrapper = styled.footer`
-  border-top: 1px solid ${palette.relatedBackground};
-  background-color: rgba(${palette.rgbContentBackground}, 0.7);
+  border-top: 3px solid ${palette.relatedBackground};
+  background-color: ${palette.lightBackground};
   width: 100vw;
   min-height: ${FOOTER_HEIGHT}px;
   z-index: 1;
@@ -19,12 +20,26 @@ const FooterWrapper = styled.footer`
   justify-content: space-between;
   ${textCss}
   padding: 1rem 0 2rem 0;
-  color: ${palette.white};
+  color: ${palette.relatedBackground};
+
+  @media (max-width: ${TABLET_LANDSCAPE_WIDTH}px) {
+    flex-direction: column;
+  }
+`;
+
+const Heading = styled.h4`
+  font-size: 1.5rem;
+  margin: 1rem 0;
+  padding: 0;
 `;
 
 const ColumnWrapper = styled.div`
   display: flex;
   flex-direction: row;
+
+  @media (max-width: ${TABLET_LANDSCAPE_WIDTH}px) {
+    flex-direction: column;
+  }
 `;
 
 const Column = styled.div`
@@ -32,15 +47,25 @@ const Column = styled.div`
   flex-direction: column;
   margin-right: 2rem;
   width: 32%;
+
+  @media (max-width: ${TABLET_LANDSCAPE_WIDTH}px) {
+    width: 100%;
+  }
 `;
 
 const Left = styled.div`
   width: 50%;
   padding-left: 1rem;
+
+  @media (max-width: ${TABLET_LANDSCAPE_WIDTH}px) {
+    width: 100%;
+    justify-content: flex-start;
+  }
 `;
 
-const Right = styled.div`
+const Right = styled(Left)`
   width: 50%;
+  padding-left: unset;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -48,10 +73,21 @@ const Right = styled.div`
   padding-right: 1rem;
 `;
 
+const Link = styled(GatsbyLink)`
+  @media (max-width: ${TABLET_LANDSCAPE_WIDTH}px) {
+    font-size: 1.5rem;
+    line-height: 1.75rem;
+  }
+`;
 const ChildLink = styled(Link)`
   margin: 0.5rem;
   font-size: 0.75rem;
   line-height: 0.8rem;
+
+  @media (max-width: ${TABLET_LANDSCAPE_WIDTH}px) {
+    font-size: 1.25rem;
+    line-height: 1.5rem;
+  }
 `;
 
 class Footer extends Component {
@@ -62,7 +98,9 @@ class Footer extends Component {
           <strong>{link.title}</strong>
         </Link>
         {link.children.map(childLink => (
-          <ChildLink key={childLink.slug} to={childLink.slug}>{childLink.title}</ChildLink>
+          <ChildLink key={childLink.slug} to={childLink.slug}>
+            {childLink.title}
+          </ChildLink>
         ))}
       </Fragment>
     );
@@ -71,7 +109,7 @@ class Footer extends Component {
     return (
       <FooterWrapper>
         <Left>
-          <h4>Developing Writers</h4>
+          <Heading>Developing Writers</Heading>
           <ColumnWrapper>
             <Column>
               <Link to="#">
@@ -84,7 +122,12 @@ class Footer extends Component {
           </ColumnWrapper>
         </Left>
         <Right>
-          <img alt="University of Michigan logo" src={logo} height="125px" width="auto" />
+          <img
+            alt="University of Michigan logo"
+            src={logo}
+            height="125px"
+            width="auto"
+          />
         </Right>
       </FooterWrapper>
     );
