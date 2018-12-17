460f1609eebb86b108e56df521c870f45942717a
blkmutt <scott.sanzenbacher@blackmuttmedia.com>
2018-12-17 10:39:14 -0500

    Updated to use new components

--- src/cms/editorComponents/Fulcrum/functions.js
+++ src/cms/editorComponents/Fulcrum/functions.js
@@ -15,8 +15,8 @@ export const audioFields = fulcrumDefaultFields;
 export const imageFields = fulcrumImageVideoFields;
 export const videoFields = fulcrumImageVideoFields;
 
-export const textAudioBlock =  (blockType, handle, title) => `<iframe data-block-type='${blockType}' src='https://www.fulcrum.org/embed?hdl=${handle}' title='${escape(title)}' style='display:block; overflow:hidden; border-width:0; width:98%; max-width:98%; max-height:400px; margin:auto'></iframe>`;
-export const imageVideoBlock = (blockType, handle, title, width) => `<div data-block-type='${blockType}' style='width:auto; page-break-inside:avoid; -webkit-column-break-inside:avoid; break-inside:avoid; max-width:${width}px; margin:auto'><div style='overflow:hidden; padding-bottom:56.25%; position:relative; height:0;'><iframe src='https://www.fulcrum.org/embed?hdl=${handle}' title='${escape(title)}' style='overflow:hidden; border-width:0; left:0; top:0; width:100%; height:100%; position:absolute;'></iframe></div></div>`;
+export const textAudioBlock =  (blockType, handle, title) => `:::md-component FulcrumTextAudio blockType="${blockType}" handle="${handle}" title="${escape(title)}"`
+export const imageVideoBlock = (blockType, handle, title, width) => `:::md-component FulcrumImageVideo blockType="${blockType}" handle="${handle}" title="${escape(title)}" width="${width}"`;
 
-export const textAudioPattern = (blockType) => new RegExp(`<iframe data-block-type='${blockType}' src='https://www.fulcrum.org/embed\\?hdl=(.*?)' title='(.*?)'[^>]*></iframe>`, 'g');
-export const imageVideoPattern = (blockType) => new RegExp(`<div data-block-type='${blockType}'[^>]*max-width:(.*?)px[^>]*><div[^>]*><iframe src='https:\\/\\/www.fulcrum.org\\/embed\\?hdl=(.*?)' title='(.*?)'[^>]*><\\/iframe><\\/div><\\/div>`, 'g');
+export const textAudioPattern = (blockType) => new RegExp(`:::md-component FulcrumTextAudio blockType="${blockType}" handle="(.*?)" title="(.*?)"`, 'g');
+export const imageVideoPattern = (blockType) => new RegExp(`:::md-component FulcrumImageVideo blockType="${blockType}" handle="(.*?)" title="(.*?)" width="(.*?)"`, 'g');
--- src/components/fulcrum/FulcrumImageVideo.js
+++ src/components/fulcrum/FulcrumImageVideo.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import { FulcrumAudio } from '../../cms/editorComponents';
+import { RefCountDisposable } from 'rx';
 
 export default class FulcrumAudio extends Component {
   render() {
-    return null;
+    const {
+      blockType,
+      handle,
+      title,
+      width,
+    } = this.props;
+
+    return (
+      <div 
+        data-block-type={blockType}
+        style={`width:auto; page-break-inside:avoid; -webkit-column-break-inside:avoid; break-inside:avoid; max-width:${width}px; margin:auto`}>
+          <div style='overflow:hidden; padding-bottom:56.25%; position:relative; height:0;'>
+            <iframe 
+              src={`https://www.fulcrum.org/embed?hdl=${handle}`}
+              title={`${escape(title)}`}
+              style='overflow:hidden; border-width:0; left:0; top:0; width:100%; height:100%; position:absolute;'>
+            </iframe>
+          </div>
+      </div>
+    );
   }
 }
