011871ef53b0d4ef46afcd13cddd60ba19fb0b9b
blkmutt <scott.sanzenbacher@blackmuttmedia.com>
2018-12-19 21:33:22 -0500

    Updated types of author fields

--- src/cms/aboutTheAuthor/AboutTheAuthor.js
+++ src/cms/aboutTheAuthor/AboutTheAuthor.js
@@ -2,11 +2,11 @@ export default {
   id: "aboutTheAuthor",
   label: "About the Author",
   fields: [
-    { name: "headhot", label: "Headshot", widget: "string" },
+    { name: "headhot", label: "Headshot", widget: "image" },
     { name: "name", label: "Name", widget: "string" },
-    { name: "bio", label: "Bio", widget: "string" },
-    { name: "chapterDescription", label: "Chapter Description", widget: "string" },
-    { name: "interviewDescription", label: "Interview Description", widget: "string" },
+    { name: "bio", label: "Bio", widget: "markdown" },
+    { name: "chapterDescription", label: "Chapter Description", widget: "markdown" },
+    { name: "interviewDescription", label: "Interview Description", widget: "markdown" },
   ],
   pattern: new RegExp(`:::md-component AboutTheAuthor headshot="(.*)" name="(.*)" bio="(.*)" chapterDescription="(.*)" interviewDescription="(.*)"$`),
   fromBlock: function(match) {
