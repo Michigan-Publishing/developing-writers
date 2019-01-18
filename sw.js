a0c9e8e5ee04c9f8fdeddafa84cca56df586d8a1
blkmutt <scott.sanzenbacher@blackmuttmedia.com>
2019-01-18 16:09:39 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,17 +26,17 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-acc5a04bf1d899830f41.js"
+    "url": "webpack-runtime-ddaaf2a3aa50952160f8.js"
   },
   {
-    "url": "app-83ea12055001aef93eba.js"
+    "url": "app-19968824e0c146857ee1.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-ab6549420b5873bef221.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "540f22004e1e0da3b6e7510e8deb9748"
+    "revision": "3a833056877870e27ac297bdb6ea9b1d"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
